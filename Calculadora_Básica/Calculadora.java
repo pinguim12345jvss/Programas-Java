750b01cba4f3f6a4e030b7fea468da7f50860e19
João Vitor Souza Santiago <joaovitor12345jvss@gmail.com>
2025-10-08 20:08:37 -0300

    Add files via upload

--- Calculadora_Básica/Calculadora.java
+++ Calculadora_Básica/Calculadora.java
@@ -3,43 +3,56 @@ import java.util.Scanner;
 public class Calculadora {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		int opcao = -1;
 
-		System.out.println("Digite o primeiro valor (a): ");
+		System.out.println("\n=== CALCULADORA JAVA ===");
+
+		System.out.println("\nDigite o primeiro valor (a): ");
 		double a = scanner.nextDouble();
 
 		System.out.println("\nDigite o segundo valor (b): ");
 		double b = scanner.nextDouble();
 
-		System.out.println("\nMenu de Opções:");
-		System.out.println("1 - Soma (a+b)");
-		System.out.println("2 - Multiplicação (a*b)");
-		System.out.println("3 - Subtração (a-b)");
-		System.out.println("4 - Divisão (a/b)");
-		System.out.println("Escolha uma opção: ");
+		while (opcao != 0) {
+			System.out.println("\n -- Menu de Opções: --\n");
+			System.out.println("1 - Soma (a+b)");
+			System.out.println("2 - Multiplicação (a*b)");
+			System.out.println("3 - Subtração (a-b)");
+			System.out.println("4 - Divisão (a/b)");
+			System.out.println("0 - Sair do Programa");
+			System.out.println("\nEscolha uma opção: \n");
+			
+			if (scanner.hasNextInt()) {
+				opcao = scanner.nextInt();
 
-		int opcao = scanner.nextInt();
-
-		switch (opcao) {
-			case 1:
-				System.out.println("A soma de a + b é: " + (a + b));
-				break;
-			case 2:
-				System.out.println("A multiplicação de a * b é: " + (a * b));
-				break;
-			case 3:
-				System.out.println("A subtração de a - b é: " + (a - b));
-				break;
-			case 4:
-			   if (b != 0) {
-				System.out.println("A divisão de a / b é: " + (a / b));
-			   } else {
-				System.out.println("Não pode dividir por zero");
+				switch (opcao) {
+					case 1:
+						System.out.println("\nA soma de a + b é: " + (a + b));
+						break;
+					case 2:
+						System.out.println("\nA multiplicação de a * b é: " + (a * b));
+						break;
+					case 3:
+						System.out.println("\nA subtração de a - b é: " + (a - b));
+						break;
+					case 4:
+			   			if (b != 0) {
+							System.out.println("\nA divisão de a / b é: " + (a / b));
+			   			} else {
+							System.out.println("\nNão pode dividir por zero");
+						}
+						break;
+					case 0:
+						System.out.println("\nSaiu do programa");
+						break;
+					default:
+						System.out.println("\n -- Opção inválida -- \n\n Selecione um dos números do menu.");
 				}
-				break;
-			default:
-				System.out.println("Opção inválida");
+			} else {
+				System.out.println("Entrada inválida, insira um número presente nas opções.");
+				scanner.next();
+			}
 		}
-
 		scanner.close();
 	}
 }
